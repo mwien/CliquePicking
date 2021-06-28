3cc6ab92d68298aa59be566e54301bb145bedcbb
Marcel Wienöbst <marcel7698@gmx.de>
2021-06-28 12:57:00 +0200

    minor change

--- counting.jl
+++ counting.jl
@@ -137,7 +137,7 @@ function mcs(G, K)
         if !visited[v]
             vertexset = Vector{Int}()
             for x in sets[maxcard]
-                visited[x] && continue
+                visited[x] && break
                 visited[x] = true
                 push!(vertexset, x)
             end
