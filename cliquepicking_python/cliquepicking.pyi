51447cdd6e841f1e8500c9c0a9dea99f16c08b1d
Marcel Wienöbst <marcel.wienoebst@gmx.de>
2025-02-07 12:00:43 +0100

    update stub file

--- cliquepicking_python/cliquepicking.pyi
+++ cliquepicking_python/cliquepicking.pyi
@@ -14,38 +14,6 @@ def mec_size(cpdag: List[Tuple[int, int]]) -> int:
     """
     ...
 
-def mec_sample_dags(
-    cpdag: List[Tuple[int, int]], k: int
-) -> List[List[Tuple[int, int]]]:
-    """
-    Sample k DAGs uniformly from the Markov equivalence class represented by the CPDAG.
-
-    Parameters
-    -----------
-    cpdag: A list of tuples representing the edges of the CPDAG, where each tuple is a pair of integers (vertex1, vertex2). Undirected edges are encoded by including both (a, b) and (b, a).
-    k: The number of DAGs to sample.
-
-    Returns
-    -------
-    A list of DAGs, where each DAG is represented as a list of tuples (edges).
-    """
-    ...
-
-def mec_sample_orders(cpdag: List[Tuple[int, int]], k: int) -> List[List[int]]:
-    """
-    Sample k DAGs (represented by a topological order) uniformly from the Markov equivalence class represented by the CPDAG.
-
-    Parameters
-    -----------
-    cpdag: A list of tuples representing the edges of the CPDAG, where each tuple is a pair of integers (vertex1, vertex2). Undirected edges are encoded by including both (a, b) and (b, a).
-    k: The number of DAGs to sample.
-
-    Returns
-    -------
-    A list of topological orders, where each topological order is represented as a list of integers (vertex indices).
-    """
-    ...
-
 def mec_list_dags(cpdag: List[Tuple[int, int]]) -> List[List[Tuple[int, int]]]:
     """
     List all DAGs in the Markov equivalence class represented by the CPDAG.
@@ -73,3 +41,8 @@ def mec_list_orders(cpdag: List[Tuple[int, int]]) -> List[List[int]]:
     A list of topological orders, where each topological order is represented as a list of integers (vertex indices).
     """
     ...
+
+class MecSampler:
+    def __init__(self, cpdag: List[Tuple[int, int]]) -> None: ...
+    def sample_dag(self) -> List[Tuple[int, int]]: ...
+    def sample_order(self) -> List[int]: ...
